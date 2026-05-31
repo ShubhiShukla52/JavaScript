199a91d321a8231d52041cf4eac4f4580a2c6431
Shubhi Shukla <2006shubhishukla@gmail.com>
2026-05-31 13:10:34 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553 //not changable value
+let accountEmail = "hitesh@google.com" //prefer to use most of the time 
+var accountPassword = "98765" //prefer not to use generally 
+accountCity = "Jaipur" //this is also valid but not in use
+let accountState; // output undefined value
+
+// accountId = 2 // not allowed
+
+accountEmail = "cc@cc.com"
+accountPassword = "26157"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
