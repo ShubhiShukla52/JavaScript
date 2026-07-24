ad84aaa2b466ec6ea93000ec46afa2881183cd02
Shubhi Shukla <2006shubhishukla@gmail.com>
2026-07-24 18:16:06 +0000

    scopes

--- .devcontainer/03_basics/02_scopes.js
+++ .devcontainer/03_basics/02_scopes.js
@@ -3,8 +3,39 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a)
+    //console.log("INNER: ", a)
 }
-console.log(a)
+//console.log(a)
 //console.log(b)
 //console.log(c)
+
+function one(){
+    const username = "shubhi"
+    
+    function two(){
+        const website = "youtube"
+        //console.log(username)
+    }
+    //console.log(website)
+
+    two()
+}
+//one()
+
+if (true) {
+    const username = "shubhi"
+    if (username === "shubhi") {
+        const website = " youtube"
+        //console.log(username + website);
+    } 
+    //console.log(website) //Output error
+}
+//console.log(username)  //Output error
+
+
+// ++++++++++++++++++++ interesting ++++++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
